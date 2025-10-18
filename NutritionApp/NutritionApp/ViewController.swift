17231495b26bc82ea0cd5bdb6fa13539ca00f73e
chinhtran <trantienchinhtop@gmail.com>
2025-10-18 11:11:23 +0700

    [18/10/2025] init pro nutrition

---
+++ NutritionApp/NutritionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NutritionApp
+//
+//  Created by Chỉnh Trần on 18/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
